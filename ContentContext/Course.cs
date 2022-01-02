4b008139df87def958d50dbe89c1a986ebf89b51
Kássia Andrade <ranimy.leitecosta@gmail.com>
2022-01-02 17:44:58 -0300

    Separating classes and creating Enums

--- ContentContext/Course.cs
+++ ContentContext/Course.cs
@@ -1,5 +1,6 @@
 using System.Reflection;
 using System.Collections.Generic;
+using MyApi.ContentContext.Enums;
 
 namespace MyApi.ContentContext
 {
@@ -11,22 +12,8 @@ namespace MyApi.ContentContext
     }
     public string Tag { get; set; }
     public IList<Module> Modules { get; set; }
-  }
+    public int DurationInMinutes { get; set; }
+    public EContentLevel Level { get; set; }
 
-  public class Module
-  {
-    public Module()
-    {
-      Lectures = new List<Lecture>();
-    }
-    public int Order { get; set; }
-    public string Title { get; set; }
-    public IList<Lecture> Lectures { get; set; }
-  }
-
-  public class Lecture
-  {
-    public int Ordem { get; set; }
-    public string Title { get; set; }
   }
 }
